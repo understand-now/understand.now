c3c86cdcc81138d71a4b9dc2ffcbe19ebedcc25e
understand-now <michael.adam@uberchristianity.com>
2026-01-18 10:36:49 -0800

    Add simple python server
    With SPA handling for 404s

---
+++ server.py
@@ -0,0 +1,13 @@
+import http.server
+
+PORT = 8888
+
+if __name__ == "__main__":
+    try:
+        with http.server.ThreadingHTTPServer(("", PORT), http.server.SimpleHTTPRequestHandler) as httpd:
+            with open("404.html") as html_file:
+                httpd.RequestHandlerClass.error_message_format = html_file.read()
+            print(f"Serving at http://127.0.0.1:{PORT}/ (Ctrl+C to stop)")
+            httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("Keyboard interrupt detected")
